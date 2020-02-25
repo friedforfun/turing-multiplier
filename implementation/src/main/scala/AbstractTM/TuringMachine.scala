3d4094dc02a0f6ee8e3ea6e7033b26414a7f1109
Sam <samfayhunt@gmail.com>
2020-02-25 17:58:52 +0000

    Updated turing machine class to use lists and aux constructor

--- implementation/src/main/scala/AbstractTM/TuringMachine.scala
+++ implementation/src/main/scala/AbstractTM/TuringMachine.scala
@@ -1,12 +1,44 @@
 package AbstractTM
 
 class TuringMachine {
-  private var _states: Array[String] = Array()
-  private var _symbols: Array[Char] = Array()
-  private var _transitions: Array[Transition] = Array()
+  class Transition(st0 : String, sy0 : Char, st1 : String, sy1 : Char, m : Char){
+    private val acceptedMoves : List[Char] = List('R', 'L', '0')
+    private val _state0 = st0
+    private val _symbol0 = sy0
+    private val _state1 = st1
+    private val _symbol1 = sy1
+    private val _move = if (acceptedMoves contains m) m else println("Invalid move value in: "+this.toString)
+
+    private val _rule = (state0, symbol0)
+    private val _result = (state1, symbol1, move)
+    private val _definition = (state0, symbol0, state1, symbol1, move)
+
+    override def toString: String = s"($state0, $symbol0, $state1, $symbol1, $move)"
+    def state0 = _state0
+    def symbol0 = _symbol0
+    def state1 = _state1
+    def symbol1 = _symbol1
+    def move = _move
+
+    def rule = _rule
+    def result = _result
+    def definition = _definition
+  }
+  // Turing machine fields
+  private var _states: List[String] = List()
+  private var _symbols: List[Char] = List()
+  private var _transitions: List[Transition] = List()
+
+  // Auxillary constructor
+  // parameter is list of states and symbols
+  def this(states: List[String], symbols: List[Char]){
+    this()
+    this._states = states
+    this._symbols = symbols
+  }
 
   def states = _states
-  def states_=(newStates: Array[String]): Unit ={
+  def states_=(newStates: List[String]): Unit ={
     _states = newStates
   }
   def addState(s : String): Unit = {
@@ -14,7 +46,7 @@ class TuringMachine {
   }
 
   def symbols = _symbols
-  def symbols_=(newSymbols: Array[Char]): Unit = {
+  def symbols_=(newSymbols: List[Char]): Unit = {
     _symbols = newSymbols
   }
 
@@ -23,7 +55,7 @@ class TuringMachine {
   }
 
   def transitions = _transitions
-  def transitions_=(newTransitions: Array[Transition]): Unit ={
+  def transitions_=(newTransitions: List[Transition]): Unit ={
     _transitions = newTransitions
   }
   def addTransition(st0 : String, sy0 : Char, st1 : String, sy1 : Char, m : Char): Unit = {
@@ -31,28 +63,6 @@ class TuringMachine {
     this.transitions :+ t
   }
 
-class Transition(st0 : String, sy0 : Char, st1 : String, sy1 : Char, m : Char){
-  private val acceptedMoves : List[Char] = List('R', 'L', '0')
-  private val _state0 = st0
-  private val _symbol0 = sy0
-  private val _state1 = st1
-  private val _symbol1 = sy1
-  private val _move = if (acceptedMoves contains m) m else println("Invalid move value in: "+this.toString)
 
-  private val _rule = (state0, symbol0)
-  private val _result = (state1, symbol1, move)
-  private val _definition = (state0, symbol0, state1, symbol1, move)
-
-  override def toString: String = s"($state0, $symbol0, $state1, $symbol1, $move)"
-  def state0 = _state0
-  def symbol0 = _symbol0
-  def state1 = _state1
-  def symbol1 = _symbol1
-  def move = _move
-
-  def rule = _rule
-  def result = _result
-  def definition = _definition
-}
 
 }
